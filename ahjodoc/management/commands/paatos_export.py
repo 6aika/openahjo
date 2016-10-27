524caa8b15573ac332e806d80a8bf8a0746d6b7e
Aapo Rista <aapo.gitpub@rista.net>
2016-10-27 08:50:56 +0300

    Add management command to export data in paatos-api import compatible format.

---
+++ ahjodoc/management/commands/paatos_export.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import os
+import logging
+import json
+
+from optparse import make_option
+from django.core.management.base import BaseCommand
+from django.db.models import ForeignKey, OneToOneField, ManyToManyField
+from mptt.models import TreeForeignKey
+from ahjodoc.models import *
+from decisions.models import *
+
+
+class Command(BaseCommand):
+    help = "Export OpenAHJO data"
+    option_list = BaseCommand.option_list + (
+        make_option('--output', dest='output', help='output filename'),
+    )
+
+    def serialize_model(self, model, exclude_fields=None):
+        self.logger.info('serializing %s (%d objects)... ' % (model.__name__, model.objects.count()))
+
+        exclude_fields = exclude_fields or []
+        all_fields = model._meta.fields + model._meta.many_to_many
+        fields = [field for field in all_fields if field.name not in exclude_fields]
+        objects = []
+
+        for obj in model.objects.prefetch_related(*(field.name for field in model._meta.many_to_many)):
+            obj_data = {}
+
+            for field in fields:
+                if type(field) in (ForeignKey, OneToOneField, TreeForeignKey):
+                    value = getattr(obj, '%s_id' % field.name)
+                    if value is not None:
+                        value = unicode(value)
+                elif type(field) == ManyToManyField:
+                    value = [i.id for i in getattr(obj, field.name).all()]
+                else:
+                    value = getattr(obj, field.name)
+                    if value is not None:
+                        value = unicode(value)
+
+                obj_data[field.name] = value
+            objects.append(obj_data)
+
+        return objects
+
+    def handle(self, *args, **options):
+        self.logger = logging.getLogger(__name__)
+        self.data_path = os.path.join(settings.PROJECT_ROOT, 'data')
+
+        self.options = options
+        objects = {}
+
+        self.logger.info('starting paatos export')
+
+        objects['meetings'] = self.serialize_model(Meeting)
+        objects['issues'] = self.serialize_model(Issue)
+        objects['agenda_items'] = self.serialize_model(AgendaItem)
+        objects['categories'] = self.serialize_model(Category, exclude_fields=['lft', 'rght', 'tree_id', 'level'])
+        objects['content_sections'] = self.serialize_model(ContentSection)
+        objects['policymakers'] = self.serialize_model(Policymaker)
+        objects['organizations'] = self.serialize_model(Organization)
+
+        self.logger.info('saving the file...')
+
+        f = open(self.options['output'], 'w')
+        f.write(json.dumps(objects))
+        f.close()
+
+        self.logger.info('paatos export done!')
